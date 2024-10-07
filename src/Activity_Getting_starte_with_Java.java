b3d692650b911866e02c8d28a82e17af69f5052b
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-08 01:00:42 +0300

    finalizing the final activity of this app code in Java

--- src/Activity_Getting_starte_with_Java.java
+++ src/Activity_Getting_starte_with_Java.java
@@ -6,24 +6,35 @@ public class Activity_Getting_starte_with_Java {
         String Movie = "The Source Code";
         int NumberOfTickets = 8;
         double PricePerTicket = 10.57;
-        System.out.println(
-                "First Name: "+ FirstName + "\n" +
-                "Last Name:  "+ LastName+ "\n" +
-                "Movie:"+ Movie + "\n" +
-                "Number of Tickets: "+ NumberOfTickets + "\n" +
-                "Price per ticket:" + PricePerTicket);
+        //System.out.println(
+             //   "First Name: "+ FirstName + "\n" +
+             //   "Last Name:  "+ LastName+ "\n" +
+             //   "Movie:"+ Movie + "\n" +
+             //   "Number of Tickets: "+ NumberOfTickets + "\n" +
+             //   "Price per ticket:" + PricePerTicket);
 
         //TODO 2: Calculate the total price based on number of tickets and price per ticket. Print the output of the total price
         double TotalPrice ;
         TotalPrice = NumberOfTickets * PricePerTicket;
-        System.out.println("Total Price of tickets: " +TotalPrice );
+        //System.out.println("Total Price of tickets: " +TotalPrice );
 
         //TODO 3: Concatenate first name and last name into new variable username with
         String username = FirstName.toLowerCase() + LastName.toLowerCase();
         // no space in between first and last name and convert them to lower case 
-        System.out.println("username: "+ username);
+        // System.out.println("username: "+ username);
         // TODO 4: Convert the movie title to upper case and assign to variable movieName 
         String MovieName = Movie.toUpperCase();
-        System.out.println(MovieName);
+        // System.out.println(MovieName);
+
+        // Final Expected out put :-
+        System.out.println("Congratulations!! You have successfully booked the tickets \n" +
+                "username: "+ username + "\n" +
+                "Movie: " + MovieName + "\n" +
+                "Number of Tickets " +NumberOfTickets +"\n" +
+                "Price Per Ticket: $"+PricePerTicket +"\n" +
+                "Total Price of 8 Tickets: $"+TotalPrice +"\n" +
+                "\n" +
+                "Thank you for choosing us for booking your movie tickets\n" +
+                "Enjoy your movie!!!");
     }
 }
