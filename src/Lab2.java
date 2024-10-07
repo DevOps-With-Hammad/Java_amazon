940d93a331c71c56fcd2e0b4e73fc1ac32da0b61
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-07 04:58:18 +0300

    Lab2 intiation

---
+++ src/Lab2.java
@@ -0,0 +1,18 @@
+public class Lab2 {
+    public static void main (String[] args) {
+        //Print the Superhero's Name and date of introduction .
+        String[] superheroes = {"Batman", "Superman", "Wonder Woman", "Spider-Man", "Iron Man"};
+        int[] introductionYears = {1939, 1938, 1941, 1962, 1963};
+        System.out.println(superheroes[0]);
+        System.out.println(superheroes[1]);
+        System.out.println(superheroes[2]);
+        System.out.println(superheroes[3]);
+        System.out.println(superheroes[4]);
+
+        System.out.println(introductionYears[0]);
+        System.out.println(introductionYears[1]);
+        System.out.println(introductionYears[2]);
+        System.out.println(introductionYears[3]);
+        System.out.println(introductionYears[4]);
+    }
+}
