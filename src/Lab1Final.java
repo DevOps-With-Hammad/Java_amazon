700b6d05932f9b80f68b0426056938d589a3093b
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-07 04:33:02 +0300

    Java programming is fun and educational

---
+++ src/Lab1Final.java
@@ -0,0 +1,7 @@
+public class Lab1Final {
+    public static void main (String [] args ) {
+        String  BooKT = " Java programming is fun and educational ";
+        System.out.println(" The Book Name is " + BooKT + "\n Does the book containee the name ' Java' ?  "
+        + BooKT.contains("Hammad") + "\n  and the shorten name of the book is\n" + "  " + BooKT.substring(0,18));
+    }
+}
