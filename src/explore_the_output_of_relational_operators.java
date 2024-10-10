935b1aaae79ebb7c509808e41cae8248437d548e
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-10 22:55:30 +0300

    Explore the output of relational operators

---
+++ src/explore_the_output_of_relational_operators.java
@@ -0,0 +1,23 @@
+public class explore_the_output_of_relational_operators {
+    //Explore the output of relational operators
+    public static void main (String [] args ){
+        //Full Gas Tank is 10
+        int CurrentGasTankLevel = 2;
+        int halfGasTankValue= 5;
+
+        if (CurrentGasTankLevel <= halfGasTankValue){
+            System.out.println("GO to Gas station ");
+        }
+        System.out.println(" On x-mas road trip ");
+        //Explore the output of relational operators
+        System.out.println(" 10 == 10: " + (10 == 10));
+        System.out.println(" 10 == 20: " + (10 == 20));
+        System.out.println(" 10 != 20: " + (10 != 20));
+        System.out.println(" 20 > 10: " + (20 > 10));
+        System.out.println(" 10 >= 20: " + (10 >= 20));
+        System.out.println(" 10 < 20: " + (10 < 20));
+        System.out.println(" 20 <= 20: " + (20 <= 20));
+
+        //
+    }
+}
