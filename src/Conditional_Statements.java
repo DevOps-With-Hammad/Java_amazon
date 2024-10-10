d7f0fdba6b72460da99ed43433047be79e066a1f
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-10 22:32:41 +0300

    Explore the output of relational operators

---
+++ src/Conditional_Statements.java
@@ -0,0 +1,14 @@
+public class Conditional_Statements {
+    public static void main (String[] args) {
+        boolean FillUpGas= true;
+
+        if (FillUpGas){
+            System.out.println("Go to the gas Station ");
+            System.out.println("FIll up the Tank  ");
+            System.out.print("Get out off the Gas station ");
+
+        }
+        System.out.println("Trip on X-max Road ");
+    }
+
+}
