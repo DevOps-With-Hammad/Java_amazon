a7e1de40b36d7777b2f6d8fb2b868defbce2ad52
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-16 02:28:34 +0300

    conditional statment with if for checking the value of the variable number is = zero or not

---
+++ src/M2_Activity_Using_conditions.java
@@ -0,0 +1,15 @@
+public class M2_Activity_Using_conditions {
+    public static void main (String [] args){
+        /* Task 1: Writing conditional statements
+            Your first task is to use an if statement to
+             check for the following conditions:
+            1.1 Check if a number is equal to zero. */
+        int number =0;
+        if (number == 1){
+            System.out.println(" The value with the number is : " +number);
+        }
+
+
+
+    }
+}
