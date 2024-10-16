48fbcdcbc28d14d8b3d17e16a55d2263c0fcada9
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-16 04:56:45 +0300

    implementing for loops

---
+++ src/M2_Implementing_for_loops.java
@@ -0,0 +1,9 @@
+public class M2_Implementing_for_loops {
+    public static void main (String [] args ){
+        int scoopsSold;
+        /*For loop */
+        for (scoopsSold = 0; scoopsSold <10 ; scoopsSold ++ ){
+            System.out.println(scoopsSold);
+        } /* Simple for loop to count form 0 to 9 */
+    }
+}
