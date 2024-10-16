c58e5979694c6a4c97ac9a878e53cf1ddff163c6
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-16 04:58:32 +0300

    small code app for for loops

--- src/M2_Implementing_for_loops.java
+++ src/M2_Implementing_for_loops.java
@@ -5,5 +5,11 @@ public class M2_Implementing_for_loops {
         for (scoopsSold = 0; scoopsSold <10 ; scoopsSold ++ ){
             System.out.println(scoopsSold);
         } /* Simple for loop to count form 0 to 9 */
-    }
+
+        System.out.print("Selling chocolate ice cream: 20 scoops");
+        for(scoopsSold=0; scoopsSold<20; scoopsSold++) {
+            System.out.print("Sold Scoop No: " + (scoopsSold + 1));
+        }
+        System.out.print("No more chocolate scoops left in box.");
+    }// Small program for FOR loop
 }
