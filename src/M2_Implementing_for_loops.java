93c98e4ba1fa60512636303da4ae2e96df41fd6b
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-16 05:09:28 +0300

    Count the number of vowels with  a declaired variable carrying  it's value

--- src/M2_Implementing_for_loops.java
+++ src/M2_Implementing_for_loops.java
@@ -11,5 +11,25 @@ public class M2_Implementing_for_loops {
             System.out.println("Sold Scoop No: " + (scoopsSold + 1));
         }
         System.out.println("No more chocolate scoops left in box.");
-    }// Small program for FOR loop
+
+
+        /** another for loop  **/
+        // The string given
+        String stringToTravel = "cinema is a great place to watch a movies  ";
+        // The variable to count the number of vowels
+        int countVowels = 0;
+        for(int indexPosition = 0; indexPosition<stringToTravel.length(); indexPosition++) {
+            // this method charAt() returns the character at the index number given
+            char ch = stringToTravel.charAt(indexPosition);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+                countVowels++;
+            }
+        }
+        System.out.println("The total number of vowels in the String is: " + countVowels);
+
+
+
+
+
+    }
 }
