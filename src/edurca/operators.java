08755c7e0dc5ba69f3ae8f0447145bd563034ef6
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2025-01-08 01:13:51 +0200

    operators shift operators

---
+++ src/edurca/operators.java
@@ -0,0 +1,22 @@
+package edurca;
+
+public class operators {
+    public static void main (String[]args){
+        int x = 10 ;
+        int y= 12;
+        int z = 14;
+
+        System.out.println(x++);
+        System.out.println(++y);
+        System.out.println(z--);
+        System.out.println(x+y);
+        System.out.println(x-y);
+
+        System.out.println(x* 2);
+        System.out.println(y / 2);
+        System.out.println(x >>y);
+        System.out.println(x<<x);
+
+    }
+
+}
